3b43da828a467f52050fe073ffcfb74ea464f1e5
Judy Lee <judyyeminlee@gmail.com>
2024-06-06 12:25:25 -0400

    finishedExercise2

--- app.js
+++ app.js
@@ -9,21 +9,25 @@ var age;
 // Exercise 2
 const firstName = "Judy";
 lastName = "Lee";
-var age = 32;
+age = 32;
+console.log(firstName, lastName, age);
 // Exercise 3
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
+console.log(language, createdYear, isCaseSensitive);
 // Exercise 4
 let price = 19.99;
 let isOnSale = false;
 let salePercentage = 15;
 let stock = 0;
-let instock = false;
+let inStock = false;
 let selectedSize = "M";
+console.log(price, isOnSale, salePercentage, stock, inStock, selectedSize);
 // Exercise 5
 let title = "Name of the Wind";
 let author = "Patrick Rothfuss";
 let pageCount = 722;
 let bookmark = 456;
 let hasRead = true;
+console.log(title, author, pageCount, bookmark, hasRead);
